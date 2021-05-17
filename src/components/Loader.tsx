25f1f897be024f7c24f0cda09dd18edfc485f27d
Brad Gibbs <bradgibbs@mac.com>
2021-05-17 17:32:55 -0500

    stuff

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,10 +1,11 @@
-import GridLoader from "react-spinners/GridLoader";
+// import GridLoader from 'react-spinners/GridLoader';
 
 const Loader: React.FC = () => {
   return (
-    <div className="container mx-auto flex flex-col h-screen justify-center items-center">
-      <GridLoader size={75} color="#eef" loading={true} />
-    </div>
+    // <div className="container mx-auto flex flex-col h-screen justify-center items-center">
+    //   <GridLoader size={75} color="#eef" loading={true} />
+    // </div>
+    <div>${console.log(`Loading...`)}</div>
   );
 };
 
