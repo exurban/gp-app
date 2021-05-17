92f15333ef15acbbe9869dbbcd66df71e09e821e
Brad Gibbs <bradgibbs@mac.com>
2021-05-17 11:09:41 -0500

    y

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -25,7 +25,7 @@ const Layout: React.FC = (props) => {
   };
 
   return (
-    <div className="bg-white dark:bg-coolGray-800">
+    <>
       <Head>
         <title>{meta.title}</title>
         <meta name="robots" content="follow, index" />
@@ -73,7 +73,7 @@ const Layout: React.FC = (props) => {
         <meta name="twitter:image" content={meta.image} key="twitterimage" />
       </Head>
       <div>
-        <header className="fixed top-0 w-screen z-10 h-24 bg-white dark:bg-coolGray-800">
+        <header className="sticky top-0 z-10 h-24 bg-white dark:bg-coolGray-800">
           <nav className="mx-4 lg:mx-10 xl:mx-20" aria-label="Top">
             <div>
               <div className="flex justify-between items-center py-2  md:space-x-8">
@@ -120,7 +120,7 @@ const Layout: React.FC = (props) => {
       </main>
 
       <Footer />
-    </div>
+    </>
   );
 };
 
